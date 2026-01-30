fc375b9feeda07f19e40b30f3931707eeb2ebd5d
Michal Niewczas <niewczas.michal96@gmail.com>
2026-01-30 09:41:01 +0100

    Usunięcie nieobsługiwanej opcji api z next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [],
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
 };
 
 export default nextConfig;
